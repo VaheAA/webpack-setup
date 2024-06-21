e69eff4ba6f3d4429be7a87fa2fb4ab99030a663
Vahe Abovyan <vahe.abov@gmail.com>
2024-06-21 22:35:03 +0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 const glob = require('glob');
 
 const mode = process.env.NODE_ENV || 'development';
@@ -10,38 +12,40 @@ const devtool = devMode && 'source-map';
 
 const htmlFiles = glob.sync('./src/**/*.html');
 
-const htmlPlugins = htmlFiles.map((file) => {
-  return new HtmlWebpackPlugin({
-    filename: path.basename(file),
-    template: file,
-    inject: true,
-    chunks: ['main'],
-  });
-});
+const htmlPlugins = htmlFiles.map((file) => new HtmlWebpackPlugin({
+  filename: path.basename(file),
+  template: file,
+  inject: true,
+  chunks: ['main'],
+}));
 
 module.exports = {
   mode,
   target,
   devtool,
-  devServer: {
-    port: 3000,
-    open: true,
-    // hot: true
-  },
   entry: [
     '@babel/polyfill',
     path.resolve(__dirname, 'src/assets/js', 'index.js'),
   ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    clean: process.env.NODE_ENV === 'production',
+    clean: true,
     filename: './assets/js/[name].[contenthash].js',
-    assetModuleFilename: './assets/img/[hash][ext]',
+    assetModuleFilename: './assets/img/[name].[contenthash][ext]',
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    hot: true, // Enable Hot Module Replacement
   },
   plugins: [
     ...htmlPlugins,
     new MiniCssExtractPlugin({
       filename: './assets/css/[name].[contenthash].css',
+    }),
+    new CleanWebpackPlugin(), // Cleans the dist folder
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(mode),
     }),
   ],
   module: {
@@ -63,59 +67,4 @@ module.exports = {
               },
             },
           },
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.woff2?$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: './assets/fonts/[name][ext]',
-        },
-      },
-      {
-        test: /\.(jpe?g|png|svg|webp|gif)$/i,
-        use: [
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                progressive: true,
-                quality: 75,
-              },
-              // optipng.enabled: false will disable optipng
-              optipng: {
-                enabled: false,
-              },
-              pngquant: {
-                quality: [0.65, 0.9],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              // the webp option will enable WEBP
-              webp: {
-                quality: 75,
-              },
-            },
-          },
-        ],
-        type: 'asset/resource',
-        generator: {
-          filename: './assets/img/[name][ext]',
-        },
-      },
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [['@babel/preset-env', { targets: 'defaults' }]],
-          },
-        },
-      },
-    ],
-  },
-};
+          '
